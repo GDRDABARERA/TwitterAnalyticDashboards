5038772d33407a0c02e582c5d1101a6baee52372
GDRDABARERA <gdrdabarera@gmail.com>
2016-03-01 17:01:53 +0530

    created dash boards

---
+++ twitter-analyticsV1/js/personCloud.js
@@ -0,0 +1,155 @@
+
+/*************************************************************************************************************
+*  Common WordCloud
+***************************************************************************************************************/
+ var dasIp="52.77.25.83";
+ var stopWords ="(trump|donaldtrump|realdonaldtrump|via|wow|10|b4|gt|get|gets|come|go|ben|carson|rubio|bencarson|cruzcrew|feelthebern|sanders|voteTrump|clinton|cruz|tedcruz|bernie|berniesanders|makeamericagreatagain|trumptrain|donald)";
+
+ var textData="";
+ var authenticateString = window.btoa("admin:user@das");
+ //"10.100.4.185"; //"52.77.25.83";
+
+
+function drawPersonWordCloud( cloudDiv ,Pname){
+
+
+
+   // var tableName= Cname+"WORDCLOUD";
+    var j=1;var i;
+
+    var stopWords ="(trump|donaldtrump|realdonaldtrump|via|wow|10|b4|gt|get|gets|come|go|ben|carson|rubio|bencarson|cruzcrew|feelthebern|sanders|voteTrump|clinton|cruz|tedcruz|bernie|berniesanders|makeamericagreatagain|trumptrain|donald)";
+
+        getPersonDataCloud( cloudDiv,Pname );
+          setInterval(function() {
+                              // Do something every 5 minutes
+                              getPersonDataCloud( cloudDiv,Pname );
+
+                        }, 300000);
+
+
+
+
+}
+
+
+
+
+function updatePersonText( new_text, stopWords, cloudDiv){
+        var cloudDivID ="#"+cloudDiv;
+            var width = $(cloudDivID).width();
+           // var widthDid = $("news").width();
+            var height = $(cloudDivID).height();
+            //console.log(width);
+                var text={
+                                  "width":width,
+                                  "height": height,
+                                  "padding": {"top":0, "bottom":0, "left":0, "right":0},
+
+                                  "data": [
+                                    {
+                                      "name": "table",
+                                      "values": [ new_text
+                                          ],
+
+                                      "transform": [
+                                        {
+                                          "type": "countpattern",
+                                          "field": "data",
+                                          "case": "upper",
+                                          "pattern": "[\\w']{3,}",
+                                          "stopwords": stopWords
+                                        },
+                                        {
+                                          "type": "formula", "field": "angle",
+                                          "expr": "[-45, 0, 45][~~(random() * 3)]"
+                                        },
+                                        {
+                                          "type": "formula", "field": "weight",
+                                          "expr": "if(datum.text=='VEGA', 600, 300)"
+                                        },
+                                        {
+                                          "type": "wordcloud",
+                                          "size": [width, height],
+                                          "text": {"field": "text"},
+                                          "rotate": {"field": "angle"},
+                                          "font": {"value": "Verdana"},
+                                          "fontSize": {"field": "count"},
+                                          "fontWeight": {"field": "weight"},
+                                          "fontScale": [20,60]
+                                        }
+                                      ]
+                                    }
+                                  ],
+
+                                  "scales": [
+                                    {
+                                      "name": "color",
+                                      "type": "ordinal",
+                                      "range":["#ff9800","#827717","#000000"]
+                                    }
+                                  ],
+
+                                  "marks": [
+                                    {
+                                      "type": "text",
+                                      "from": {"data": "table"},
+                                      "properties": {
+                                        "enter": {
+                                          "x": {"field": "layout_x"},
+                                          "y": {"field": "layout_y"},
+                                          "angle": {"field": "layout_rotate"},
+                                          "font": {"field": "layout_font"},
+                                          "fontSize": {"field": "layout_fontSize"},
+                                          "fontStyle": {"field": "layout_fontStyle"},
+                                          "fontWeight": {"field": "layout_fontWeight"},
+                                          "text": {"field": "text"},
+                                          "align": {"value": "center"},
+                                          "baseline": {"value": "alphabetic"},
+                                          "fill": {"scale": "color", "field": "text"}
+                                        },
+                                        "update": {
+                                          "fillOpacity": {"value": 1}
+                                        },
+                                        "hover": {
+                                          "fillOpacity": {"value": 0.5}
+                                        }
+                                      }
+                                    }
+                                  ]
+
+                     };
+
+
+            return text;
+
+}
+
+function getPersonDataCloud(cloudDiv, Pname){
+        var cloudDivID="#"+cloudDiv;
+        var newTestString=" ";
+        var Candidates = { Choose : Pname};
+                $.ajax({
+                         url: "js/candidateCloud.jag",
+                         dataType: "json",
+                         contentType:'application/json',
+                         data: JSON.stringify(Candidates),
+                         type: "POST",
+                         success: function (data) {
+                                                        //    console.log(TextData);
+                                            var TextData=JSON.stringify(data);
+                                                                new_cloud =updatePersonText(TextData, stopWords,cloudDiv);
+                                                                 var viewUpdateFunction = (function(chart) {
+                                                               	        this.view = chart({el:cloudDivID}).update();
+                                                                 }).bind(this);
+                                                                 vg.parse.spec(new_cloud, viewUpdateFunction);
+
+
+                                                           }
+
+                                               });
+
+
+
+}
+
+
---
+++ wso2-con-analyticsV1/js/main.old.js
@@ -0,0 +1,373 @@
+var dasIp="52.77.25.83";
+
+var authenticatingString= window.btoa("admin:user@con2016");
+
+
+
+
+
+(function () {
+
+	'use strict';
+
+	// iPad and iPod detection
+	var isiPad = function () {
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function () {
+		return (
+			(navigator.platform.indexOf("iPhone") != -1) ||
+			(navigator.platform.indexOf("iPod") != -1)
+		);
+	};
+
+	// Full height
+	var fullHeight = function () {
+		if (!isiPhone() || !isiPad()) {
+			$('.js-full-height').css('height', $(window).height());
+			$(window).resize(function () {
+				$('.js-full-height').css('height', $(window).height());
+			});
+		}
+	};
+
+	// Scroll Next
+	var ScrollNext = function () {
+		$('body').on('click', '.scroll-btn', function (e) {
+			e.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $($(this).closest('[data-next="yes"]').next()).offset().top
+			}, 1000, 'easeInOutExpo');
+			return false;
+		});
+	};
+
+	// Parallax
+	var parallax = function () {
+		$(window).stellar();
+	};
+
+	// Counter
+	counter = function () {
+		$('.fh5co-counter-style-1').waypoint(function (direction) {
+			var el = $(this.element).attr('class');
+			if (direction === 'down' && !$(this.element).hasClass('animated')) {
+				setTimeout(function () {
+					// console.log($(this.element));
+					$('.' + el).find('.js-counter').countTo({
+						formatter: function (value, options) {
+							return value.toFixed(options.decimals);
+						},
+					});
+				}, 200);
+
+				$(this.element).addClass('animated');
+
+			}
+		}, {offset: '75%'});
+
+
+		$('.fh5co-counter-style-2').waypoint(function (direction) {
+			var el = $(this.element).attr('class');
+			if (direction === 'down' && !$(this.element).hasClass('animated')) {
+				setTimeout(function () {
+					$('.' + el).find('.js-counter').countTo({
+						formatter: function (value, options) {
+							return value.toFixed(options.decimals);
+						},
+					});
+				}, 200);
+
+				$(this.element).addClass('animated');
+
+			}
+		}, {offset: '75%'});
+	};
+
+	// Click outside of offcanvass
+	var mobileMenuOutsideClick = function () {
+		$(document).click(function (e) {
+			var container = $("#fh5co-offcanvass, .js-fh5co-mobile-toggle");
+			if (!container.is(e.target) && container.has(e.target).length === 0) {
+				$('html').removeClass('mobile-menu-expanded');
+				$('.js-fh5co-mobile-toggle').removeClass('active');
+			}
+		});
+	};
+
+	// Burger Menu
+	var burgerMenu = function () {
+
+		$('body').on('click', '.js-fh5co-nav-toggle', function (event) {
+			if ($('#navbar').is(':visible')) {
+				$(this).removeClass('active');
+			} else {
+				$(this).addClass('active');
+			}
+			event.preventDefault();
+		});
+
+	};
+
+	// Off Canvass
+	var offCanvass = function () {
+
+		if ($('#fh5co-offcanvass').length == 0) {
+			if ($('.fh5co-nav-style-1').length > 0) {
+				$('body').prepend('<div id="fh5co-offcanvass" />');
+
+				$('.fh5co-link-wrap').each(function () {
+					$('#fh5co-offcanvass').append($(this).find('[data-offcanvass="yes"]').clone());
+				})
+				$('#fh5co-offcanvass').find('.js-fh5co-mobile-toggle').remove();
+				$('#fh5co-offcanvass, #fh5co-page').addClass($('.fh5co-nav-style-1').data('offcanvass-position'));
+				$('#fh5co-offcanvass').addClass('offcanvass-nav-style-1');
+			}
+
+			if ($('.fh5co-nav-style-2').length > 0) {
+				$('body').prepend('<div id="fh5co-offcanvass" />');
+
+				$('.fh5co-link-wrap').each(function () {
+					$('#fh5co-offcanvass').append($(this).find('[data-offcanvass="yes"]').clone());
+				})
+				$('#fh5co-offcanvass').find('.js-fh5co-mobile-toggle').remove();
+				$('#fh5co-offcanvass, #fh5co-page').addClass($('.fh5co-nav-style-2').data('offcanvass-position'));
+				$('#fh5co-offcanvass').addClass('offcanvass-nav-style-2');
+			}
+		}
+
+		$('body').on('click', '.js-fh5co-mobile-toggle', function (e) {
+			var $this = $(this);
+			$this.toggleClass('active');
+			$('html').toggleClass('mobile-menu-expanded');
+
+		});
+
+		if ($(window).width() < 769) {
+			$('body, html').addClass('fh5co-overflow');
+		}
+
+		$(window).resize(function () {
+			if ($(window).width() < 769) {
+				$('body, html').addClass('fh5co-overflow');
+			}
+			if ($(window).width() > 767) {
+				if ($('html').hasClass('mobile-menu-expanded')) {
+					$('.js-fh5co-mobile-toggle').removeClass('active');
+					$('html').removeClass('mobile-menu-expanded');
+				}
+			}
+		});
+
+	};
+
+
+	// Magnific Popup
+
+	var imagePopup = function () {
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			removalDelay: 10,
+			titleSrc: 'title',
+			gallery: {
+				enabled: false
+			}
+		});
+	};
+
+
+	// Window Scroll
+	var windowScroll = function () {
+		var lastScrollTop = 0;
+
+		$(window).scroll(function (event) {
+
+			var header = $('#fh5co-header'),
+				scrlTop = $(this).scrollTop();
+
+			if (scrlTop > 500 && scrlTop <= 2000) {
+				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
+			} else if (scrlTop <= 500) {
+				if (header.hasClass('navbar-fixed-top')) {
+					header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
+					setTimeout(function () {
+						header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
+					}, 100);
+				}
+			}
+
+		});
+	};
+
+//web-socket for speach beta section
+
+	var checkCEPStatus = function () {
+		var ws = new WebSocket("wss://52.77.25.83:9446/outputwebsocket/VoiceSocket");
+		ws.onopen = function () {
+			console.log('Socket open');
+		}
+		ws.onerror = function (error) {
+			console.log(error);
+		}
+		ws.onmessage = function (e) {
+			var msg = JSON.parse(e.data);
+			if (msg.event.payloadData.name == "FINISH") {
+				var speakerImg = msg.event.payloadData.Pic; //add the token
+				var WordContainer = "";
+				var spName = msg.event.payloadData.SpeakerName;
+				var spTitle = msg.event.payloadData.Title; //title speech
+				var spDescription = msg.event.payloadData.dis;
+				var comingList = ["API", "Analytics", "AppDev", "Cloud", "Governance", "IOT", "Identity", "Integration", "Microservices", "Vega", "WSO2Con"];
+
+				for (var i = 0; i < comingList.length; i++) {
+
+					var n = msg.event.payloadData[comingList[i]],
+						wrd = comingList[i];
+					if (n != 0 || n != '0') {
+						var Tword = " <div class='word-container'><div class='word-count'> <h2 class='count' id='TwordCount'>" + n + "</h2> <span>times</span> <div class='clearfix'></div>  </div>  <h3 class='word' id='Tword_word'>" + wrd + "</h3>  </div> ";
+
+						WordContainer += Tword;
+					}
+				}
+
+				var element = "<li class='speech-stat-container'>" +
+					" <div class='fh5co-cover fh5co-cover-style-2 js-full-height' data-stellar-background-ratio='0.5' data-next='yes'> " +
+					"<div class='fh5co-overlay'></div>" +
+					"<div class='fh5co-cover-text'> " +
+					"<div class='container-fluid'> " +
+					"<div class='row'>" +
+					"<div class='col-md-12 full-height js-full-height'>" +
+					"<div class='fh5co-cover-intro'>" +
+					"<h1 class='cover-text-lead wow fadeInUp' data-wow-duration='1s' data-wow-delay='.5s'>Speech Stats the speech</h1>" +
+					"<h5 class='wow fadeInUp beta-tag' data-wow-duration='1s' data-wow-delay='.9s'>Beta</h5> " +
+					"<div class='graph-container speech-stat'> " +
+					"<img class='preserve' src='" + speakerImg + "' />" +
+					"<div class='stat-container'>" +
+					"<h2>You just listened to <span class='speaker-name' >" + spName + "</span></h2> " +
+					"<h3 id='speakerTitle'>" + spTitle + "</h3> " +
+					"<p  id='speakerDescription'>" + spDescription + "</p>" +
+					"<div class='word-count-container'>" + WordContainer + "</div>" +
+					"</div> " +
+					"</div> " +
+					"</div> " +
+					"</div> " +
+					"</div>  " +
+					"</div>" +
+					"</div> " +
+					"</div>  " +
+					"</li>";
+
+
+				$('.slides-container').append(element);
+				$('#page-container').superslides('update');
+				$('#page-container').superslides('animate', 4);
+				$('#page-container').superslides('start');
+			} else if (msg.event.payloadData.name == "START" && $('.speech-stat-container').length != 0) {
+				$('.speech-stat-container').remove();
+				$('#page-container').superslides('update');
+				$('#page-container').superslides('animate', 0);
+				$('#page-container').superslides('start');
+
+			}
+		}
+
+
+	};
+
+
+	// Document on load.
+	$(function () {
+
+		fullHeight();
+
+
+		$('#page-container').superslides({
+			play: 10000,
+		});
+		ajaxLatest();
+		Hashtag();
+
+		checkCEPStatus();
+
+
+		setInterval(function () {
+			ajaxLatest();
+			Hashtag();
+		}, 2000);
+
+
+		getTop5();
+
+		$('#page-container').on('animating.slides', function (e, t) {
+			//console.log(t.upcoming_slide);
+			if (t.upcoming_slide == 0) {
+
+			} else if (t.upcoming_slide == 1) {
+				drawWordCloud("CommonWordCloud");
+			} else if (t.upcoming_slide == 2) {
+				$('#CommunityGraph').css('display', 'block');
+				$('#CommunityGraph').width('100%');
+				setTimeout(function () {
+					drawBubbleGraph("BubblePopularTopic")
+				}, 500);
+			} else if (t.upcoming_slide == 3) {
+				$('#CommunityGraph').css('display', 'block');
+				$('#CommunityGraph').width(t.upcoming_slide_width / 1.7);
+
+				communityGraph("CommunityGraph");
+				getTop5();
+
+			}
+		})
+	});
+
+
+//-----------top5
+
+
+	function getTop5() {
+
+		$.ajax({
+
+			url: "https://52.77.25.83:9446/analytics/tables/TOP5",
+			beforeSend: function (xhr) {
+				xhr.setRequestHeader("Authorization", "Basic " + authenticatingString);
+			},
+			method: "GET",
+
+			contentType: "application/json",
+			success: function (top) {
+				//	console.log(JSON.stringify(top));
+				var j = 1;
+				for (var i = 0; i < 5; i++) {
+
+					var nameID = "#ID" + (j);
+					var imageID = "#img" + (j);
+					var fullNameID = "#Fname" + (j);
+
+					var countID = "#count" + (j);
+					j++;
+
+					var url = "https://twitter.com/" + top[i].values.name + "/profile_image?size=original";
+
+					var tname = "@" + top[i].values.name;
+
+					//  $(imageID).attr("src",url);
+
+					$(fullNameID).html(top[i].values.fullName);
+					$(nameID).html(tname);
+					$(countID).html(top[i].values.degree);
+
+
+				}
+
+			}
+
+
+		});
+
+
+	}
+})();
